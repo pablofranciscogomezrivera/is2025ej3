6171dc66c0738faee2405dffda5485c51d1e407a
Pablo-fgr <pablito03121@hotmail.es>
2025-11-06 21:26:49 -0300

    feat: Agrega atributos Tax y FinalPrice a Product

--- productapi/src/main/java/is2025/ut4/productapi/domain/Product.java
+++ productapi/src/main/java/is2025/ut4/productapi/domain/Product.java
@@ -5,14 +5,17 @@ public class Product {
     private String description;
     private double price;
     private int stock;
+    private double tax;
+    private double finalPrice;
 
     public Product() {}
 
-    public Product(String code, String description, double price, int stock) {
+    public Product(String code, String description, double price, int stock, double tax) {
         this.code = code;
         this.description = description;
         this.price = price;
         this.stock = stock;
+        this.tax = tax;
     }
 
     public String getCode() { return code; }
@@ -23,6 +26,18 @@ public class Product {
 
     public double getPrice() { return price; }
     public void setPrice(double price) { this.price = price; }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getFinalPrice() {
+        return this.price * this.tax;
+    }
 
     public int getStock() { return stock; }
     public void setStock(int stock) { this.stock = stock; }
